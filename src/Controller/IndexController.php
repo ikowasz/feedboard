526d35c2b0307c8a862320987bd60bfd618c2262
ikowasz <dev.ikowasz@gmail.com>
2025-03-30 21:50:03 +0200

    index added

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class IndexController extends AbstractController
+{
+    #[Route('/', name: 'app_index')]
+    public function index(): Response
+    {
+        if (!$this->getUser()) {
+            return $this->redirectToRoute('app_login');
+        }
+        return $this->redirectToRoute('app_feed');
+    }
+}
